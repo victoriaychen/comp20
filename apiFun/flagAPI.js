3b077729c895842a15784b7eb2df7eb6ceadcd70
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-03-26 20:11:32 -0700

    Updated files

--- apiFun/flagAPI.js
+++ apiFun/flagAPI.js
@@ -1,4 +1,6 @@
 const input = document.getElementById("countryname");
+
+//Add an event listern for when a key is pressed
 input.addEventListener("keyup", findFlag);
 
 async function findFlag(event) {
@@ -6,10 +8,14 @@ async function findFlag(event) {
   const URL = `https://restcountries.eu/rest/v2/name/${countryname}?fullText=true`;
   console.log(URL);
 
+    //send network requests to the server
     const response = await fetch(URL);
+
+    //Get the API json files
     const countryData = await response.json();
     console.log("Data: ", countryData);
 
+    //display the flag data
     const result = document.getElementById("flag");
     result.innerHTML = `
         <img src=${countryData[0].flag} style="width: 30%; height: 30%">
